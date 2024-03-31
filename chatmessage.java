355d3858bb37fcd74cbd2d6cea805223ff01b658
Niharika141 <124150841+Niharika141@users.noreply.github.com>
2024-03-31 21:25:07 +0530

    Create chatmessage.java

---
+++ chatmessage.java
@@ -0,0 +1,28 @@
+public class ChatMessage {
+    private String content;
+    private String sender;
+    // getters and setters
+}
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat").withSockJS();
+    }
+}
+
+@Controller
+public class ChatController {
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+}
